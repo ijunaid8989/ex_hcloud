220cde3dbb99b028d2f2c53366272f81b4795531
Junaid Farooq <junaid@evercam.io>
2018-08-23 20:31:11 +0500

    Added HTTPoison.

--- lib/hcloud.ex
+++ lib/hcloud.ex
@@ -1,18 +1,2 @@
 defmodule HetznerCloud do
-  @moduledoc """
-  Documentation for HetznerCloud.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HetznerCloud.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
