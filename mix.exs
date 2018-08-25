c63a9fb459fb8d026d1f943b90254c67c7f97cae
Junaid Farooq <junaid@evercam.io>
2018-08-25 22:08:43 +0500

    Added ex_doc.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,20 @@ defmodule HetznerCloud.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description()
+    ]
+  end
+
+  def description, do: "HetznerCloud client for Elixir"
+
+  def package do
+    [
+      name: :hcloud,
+      maintainers: ["Junaid Farooq"],
+      licenses: ["MIT"],
+      docs: [extras: ["README.md"]],
+      links: %{"GitHub" => "https://github.com/ijunaid8989/ex_hcloud"}
     ]
   end
 
@@ -20,7 +33,8 @@ defmodule HetznerCloud.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.2"},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 4.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
